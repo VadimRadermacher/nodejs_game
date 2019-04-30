5a9b972ae35e27ca60d533097d75c3c7a09a0115
joe-michel <joe.michel.f@gmail.com>
2019-04-30 10:42:56 +0200

    A lot of things

---
+++ server/Game.js
@@ -0,0 +1,10 @@
+const uuidv4 = require('uuid/v4');
+const _ = require('lodash');
+
+module.exports = class Game {
+  constructor() {
+    this.id = uuidv4();
+    this.players = [null, null];
+    this.reset();
+  }
+}
